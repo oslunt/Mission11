cc1f3cab0212a70bffae6f7a51d3dce1e2d591bd
oslunt <77417770+oslunt@users.noreply.github.com>
2022-03-09 14:20:14 -0700

    Initial Commit

--- Models/Purchase.cs
+++ Models/Purchase.cs
@@ -31,5 +31,8 @@ namespace Mission7.Models
 
         [Required(ErrorMessage = "Please enter a country")]
         public string Country { get; set; }
+
+        [BindNever]
+        public bool PurchaseShipped { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -41,6 +41,8 @@ namespace Mission7
 
             services.AddScoped<Cart>(x => SessionCart.GetCart(x));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddServerSideBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -74,6 +76,9 @@ namespace Mission7
                 endpoints.MapDefaultControllerRoute();
 
                 endpoints.MapRazorPages();
+
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
             });
         }
     }
